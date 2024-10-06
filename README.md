b6fd47e77d1d2136d04d13f50e2c0c2f1efbf5e6
antongenchev <antongenchevfractal@gmail.com>
2024-10-06 18:18:03 +0300

    Add a function for generating hashes given a list of peaks

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # audio-match
 A full stack web app for audio recognition
+
+-Redis container for the task queue
+-Celery for the task workers. which will process audio and handle more
+time consuming audio matching
+-Flask app for the backend. Initially flask will render templates but
+later a separate frontend app can be created
+-Postgres database for storing the users, the acoustic fingerprints,
+etc.
+-PgAdmin for having an easy to use interface to the database
+-Flower for having an easy view of the celery tasks while developing
+
+For now the audio files will be stored on a docker volume. The plan is
+to move them to an S3 bucket later on
---
+++ src/hashing.py
@@ -0,0 +1,32 @@
+import binascii
+import hashlib
+from typing import List, Tuple
+
+def generate_hashes(peaks:List[Tuple[int, int]], fan_value:int=10) -> List:
+    '''
+    Generate hashes given a list of peaks and a fan_value.
+    Each hash is generate using the time difference of two peaks and their frequenct bins
+
+    Parameters:
+        peaks: a list of tuples, each tuple representing a single peak.
+            The tuples consist of two integers: the time bin and the frequency bin
+        fan_value: an integer parameter describing how close two peaks have to create a hash.
+            fan_value=10 means that each peak will be considered as a pair with its 9 closest neighbours.
+
+    Returns:
+        A list
+    '''
+    hashes = []
+    for i in range(len(peaks)):
+        for j in range(1, fan_value):
+            # Consider each pair of peaks (i,i+1), (i,i+2), ..., (i,i+fan_value-1)
+            if (i + j) < len(peaks): # Make sure we don't index out of range
+                f1 = peaks[i][1]
+                f2 = peaks[i + j][1]
+                t1 = peaks[i][0]
+                t2 = peaks[i + j][0]
+                t_delta = t2 - t1
+                # Calcuate the SHA-1 hash of the peak pair
+                h = hashlib.sha1(f"{str(f1)}{str(f2)}{str(t_delta)}".encode('utf-8'))
+                hashes.append((h.hexdigest(), t1)) # sha1 hash = 20 bytes -> 40 hexadecimal symbols
+    return hashes
